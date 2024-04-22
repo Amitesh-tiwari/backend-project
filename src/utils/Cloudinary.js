81e68c20b812a5f22746090559c2ff59097cdac7
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-04-23 01:11:47 +0530

    Integrated the multer for file storage

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (LocalFilePath) => {
+    try{
+        if(!LocalFilePath) return null;
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(LocalFilePath,{
+            resource_type: "auto",
+        })
+        //file has been uploaded successfully
+        console.log("File uploaded successfully",response.url);
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(LocalFilePath);//remove the locally saved file when the uploading operation failed.
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
