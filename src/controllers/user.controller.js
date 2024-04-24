a3997fc1b249aaa3d2fed4423815b721e55297e5
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-04-24 11:12:00 +0530

    steps completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,10 @@ const registerUser = asyncHandler(async (req, res) => {
         //checking the image and the avatar image
         //uploading them ion the cloudinary
         //saving the user data in the database
+        //creating user object- create entry in the database
+        //remove password and refresh token from the response
+        //check for user creation
+        //return the response
 });
 
 export default registerUser;
