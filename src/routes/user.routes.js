53427602abd70fd813e0f4362414a9ba7bf00bc4
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-05-03 14:22:38 +0530

    Add new user-related endpoints to user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -30,4 +30,36 @@ router.post("/logout", verifyJWT, (req, res) => {
     res.send(logoutUser);
 });
 
+router.route("/refresh-token").post((req, res) => {
+    res.send(refreshAccessToken);
+});
+
+router.route("/change-password").post(verifyJWT, (req, res) => {
+    res.send(changePassword); 
+});
+
+router.route("/current-user").get(verifyJWT, (req, res) => {
+    res.send(getCurrentUser);
+});
+
+router.route("/update-account").patch(verifyJWT, (req, res) => {
+    res.send(updateAccountDetails);
+});
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), (req, res) => {
+    res.send(updateUserAvatar);
+});
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"), (req, res) => {
+    res.send(updateUserCoverImage);
+});
+
+router.route("/c/.username").get(verifyJWT, (req, res) => {
+    res.send(getUserChannelProfile);
+});
+
+router.route("/history").get(verifyJWT, (req, res) => {
+    res.send(getWatchHistory);
+});
+
 export default router; 
